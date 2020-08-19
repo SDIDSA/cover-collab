0d48a555aa224edb9dd0739cfe7ad9bf768dfc17
sdidsa <zinelabidine.teyar@univ-constantine2.dz>
2020-08-19 22:44:44 +0100

    Updates

---
+++ precache-manifest.670c7921f5f54d99bd421982d88edd89.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3595d2c2643ae06bdc63aa1c2c5375b4",
+    "url": "/cover-collab/index.html"
+  },
+  {
+    "revision": "5d9309a8a04448454eba",
+    "url": "/cover-collab/static/css/main.765cca4d.chunk.css"
+  },
+  {
+    "revision": "d5c932f8c76f2e7ba579",
+    "url": "/cover-collab/static/js/2.fb9d5f7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cover-collab/static/js/2.fb9d5f7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9309a8a04448454eba",
+    "url": "/cover-collab/static/js/main.9f4469bc.chunk.js"
+  },
+  {
+    "revision": "01ad7f64f12032c2f7da",
+    "url": "/cover-collab/static/js/runtime-main.3c86fbb4.js"
+  },
+  {
+    "revision": "39d9d460408d23e119ce1ff00d97d0ae",
+    "url": "/cover-collab/static/media/add.39d9d460.svg"
+  },
+  {
+    "revision": "b2451b1f771053855b741052c27c21ac",
+    "url": "/cover-collab/static/media/empty.b2451b1f.svg"
+  },
+  {
+    "revision": "ca7a2f09af5b17ee11fecdaa522ae4bd",
+    "url": "/cover-collab/static/media/logo.ca7a2f09.svg"
+  }
+]);
